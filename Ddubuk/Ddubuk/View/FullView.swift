dd46520e01f5cbe4fa186a38553248432092fb97
ghrjs1998 <zbsl4547@naver.com>
2024-01-29 16:24:17 +0900

    디테일 뷰 구현

---
+++ Ddubuk/Ddubuk/View/FullView.swift
@@ -0,0 +1,18 @@
+//
+//  FullView.swift
+//  Ddubuk
+//
+//  Created by 박호건 on 1/29/24.
+//
+
+import SwiftUI
+
+struct FullView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FullView()
+}
