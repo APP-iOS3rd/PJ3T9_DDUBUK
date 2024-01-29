9686bbd39c9cf4813f509ec5a243e43049648ac7
ghrjs1998 <zbsl4547@naver.com>
2024-01-29 17:01:03 +0900

    Search뷰 구현

--- Ddubuk/SearchView.swift
+++ Ddubuk/SearchView.swift
@@ -2,14 +2,40 @@
 //  SearchView.swift
 //  Ddubuk
 //
-//  Created by 박호건 on 1/29/24.
+//  Created by 박호건 on 1/22/24.
 //
 
 import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("산책하는 지역명을 검색해주세요")
+                    .fontWeight(.semibold)
+            }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundColor(.black)
+            }
+
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundColor(.gray)
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
     }
 }
 
