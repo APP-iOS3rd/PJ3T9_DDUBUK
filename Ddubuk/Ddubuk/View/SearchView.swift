69995728ca13e8631ca7f55139da650bd1a4ed4e
ghrjs1998 <zbsl4547@naver.com>
2024-01-31 22:14:14 +0900

    MapView 오류 해결

--- Ddubuk/Ddubuk/View/SearchView.swift
+++ Ddubuk/Ddubuk/View/SearchView.swift
@@ -9,33 +9,35 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        HStack {
-            Image(systemName: "magnifyingglass")
-            
-            VStack(alignment: .leading, spacing: 2) {
-                Text("산책하는 지역명을 검색해주세요")
-                    .fontWeight(.semibold)
-            }
-            
-            Spacer()
-            
-            Button {
+        VStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
                 
-            } label: {
-                Image(systemName: "line.3.horizontal.decrease.circle")
-                    .foregroundColor(.black)
-            }
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("산책하는 지역명을 검색해주세요")
+                        .fontWeight(.semibold)
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "line.3.horizontal.decrease.circle")
+                        .foregroundColor(.black)
+                }
 
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 10)
+            .overlay {
+                Capsule()
+                    .stroke(lineWidth: 0.5)
+                    .foregroundColor(.gray)
+                    .shadow(color: .black.opacity(0.4), radius: 2)
+            }
+            .padding()
         }
-        .padding(.horizontal)
-        .padding(.vertical, 10)
-        .overlay {
-            Capsule()
-                .stroke(lineWidth: 0.5)
-                .foregroundColor(.gray)
-                .shadow(color: .black.opacity(0.4), radius: 2)
-        }
-        .padding()
     }
 }
 
