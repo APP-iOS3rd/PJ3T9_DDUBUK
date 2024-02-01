53e2933300a9d1d872deebec0fc108f1eb88ca7a
ghrjs1998 <zbsl4547@naver.com>
2024-02-01 19:05:36 +0900

    메인뷰 구현

--- Ddubuk/Ddubuk/View/SearchView.swift
+++ Ddubuk/Ddubuk/View/SearchView.swift
@@ -8,36 +8,58 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var title: String = ""
+    
+    
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                
-                VStack(alignment: .leading, spacing: 2) {
-                    Text("산책하는 지역명을 검색해주세요")
-                        .fontWeight(.semibold)
+        ScrollView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    
+                    VStack(alignment: .leading, spacing: 2) {
+                        TextField("산책하는 지역명을 검색해주세요", text: $title)
+                            
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal.decrease.circle")
+                            .foregroundColor(.black)
+                    }
+
                 }
+                .padding(.horizontal)
+                .padding(.vertical, 10)
+                .overlay {
+                    Capsule()
+                        .stroke(lineWidth: 0.5)
+                        .foregroundColor(.gray)
+                        .shadow(color: .black.opacity(0.4), radius: 2)
+                }
+                .padding()
                 
                 Spacer()
                 
-                Button {
-                    
-                } label: {
-                    Image(systemName: "line.3.horizontal.decrease.circle")
-                        .foregroundColor(.black)
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 10) {
+                        MainListView()
+                    }
+                    .padding()
                 }
-
+                
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 10) {
+                        MainListView()
+                    }
+                    .padding()
+                }
             }
-            .padding(.horizontal)
-            .padding(.vertical, 10)
-            .overlay {
-                Capsule()
-                    .stroke(lineWidth: 0.5)
-                    .foregroundColor(.gray)
-                    .shadow(color: .black.opacity(0.4), radius: 2)
-            }
-            .padding()
         }
+        .padding()
     }
 }
 
