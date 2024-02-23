67e0a7049031c84f76bd40a5b4c699191811919e
minsik <olivetree421@gmail.com>
2024-02-23 14:29:54 +0900

    Edit: map 현재위치

--- Ddubuk/Ddubuk/Map/View/MapView.swift
+++ Ddubuk/Ddubuk/Map/View/MapView.swift
@@ -14,41 +14,46 @@ struct MapView: View {
     
     @ObservedObject var routes = FireStoreManager.shared
     @State private var selection: UUID?
+    @State private var position: MapCameraPosition = .automatic
+    @StateObject var locationManager = LocationManager()
+    @EnvironmentObject var viewModel: RecordViewModel
     
     var body: some View {
         ZStack {
             //
-            Map(initialPosition: .region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.575000, longitude: 127.089390), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))), selection: $selection) {
+            Map(initialPosition: position, selection: $selection) {
                 ForEach(routes.routes) { location in
                     Marker("", coordinate: CLLocationCoordinate2D(latitude: location.coordinates[0].latitude, longitude: location.coordinates[0].longitude))
                         .tint(.blue)
                     
                 }
+                
                 if let selection {
                     if let item = routes.routes.first(where: { $0.id == selection }) {
-//                        ForEach(item.coordinates) { coordinate in
-//                            locations.append(CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude))
-//                        }
+                        //                        ForEach(item.coordinates) { coordinate in
+                        //                            locations.append(CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude))
+                        //                        }
                         let coordinates = item.coordinates.map { CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) }
                         MapPolyline(coordinates: coordinates)
                             .stroke(.blue, lineWidth: 3.0)
                     }
                 }
             }
-            VStack {
-                Spacer()
-                
-                if let selection {
-                    if let item = routes.routes.first(where: { $0.id == selection }) {
-                        RoundedRectangle(cornerRadius: 10)
-                            .padding([.bottom, .horizontal])
-                            .frame(width: 350, height: 150)
-                            .foregroundColor(.white)
-                            .overlay (
-                                MapMarkerDetail(seletedResult: item)
-                                    .frame(height: 128)
-                            )
-                    }
+        }
+        VStack {
+            Spacer()
+            
+            if let selection {
+               
+                if let item = routes.routes.first(where: { $0.id == selection }) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .padding([.bottom, .horizontal])
+                        .frame(width: 350, height: 150)
+                        .foregroundColor(.white)
+                        .overlay (
+                            MapMarkerDetail(seletedResult: item)
+                                .frame(height: 128)
+                        )
                 }
             }
         }
@@ -56,8 +61,10 @@ struct MapView: View {
             routes.fetchRoutes()
         }
     }
-    
+        
 }
+
+
 
 //func changeToCLL2D(_ location: [Coordinate]){
 //    location.forEach { coordinate in
